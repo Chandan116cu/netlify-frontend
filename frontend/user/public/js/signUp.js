dd00775530d50a68e013d05a6ee5c000605b71d2
Chandan <ch.cse16@chitkarauniversity.edu.in>
2019-11-22 16:26:45 +0530

    changes in phonenumber

--- frontend/user/public/js/signUp.js
+++ frontend/user/public/js/signUp.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
             data: JSON.stringify({
                 "name": name,
                 "email": Email,
-                "phoneNumber": PhoneNumber,
+                "phoneNumber": '+91'+PhoneNumber,
                 "password": Password,
                 "accountType": accountType
             }),
